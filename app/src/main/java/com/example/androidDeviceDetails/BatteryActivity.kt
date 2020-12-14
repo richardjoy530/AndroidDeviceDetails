8fc59c8dec706c56572bfee2ca49ee416c620478
Richard Joy <richardjoy530@gmail.com>
2020-12-14 12:07:12 +0530

    UI optimised bug fixed

--- app/src/main/java/com/example/androidDeviceDetails/BatteryActivity.kt
+++ app/src/main/java/com/example/androidDeviceDetails/BatteryActivity.kt
@@ -3,13 +3,14 @@ package com.example.androidDeviceDetails
 import android.os.Bundle
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.androidDeviceDetails.adapters.BatteryListAdapter
 import com.example.androidDeviceDetails.managers.AppBatteryUsageManager
+import com.example.androidDeviceDetails.managers.AppEntry
 
 
 class BatteryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         setContentView(R.layout.activity_battery)
         val batteryListView = findViewById<ListView>(R.id.batteryListView)
         AppBatteryUsageManager().cookBatteryData(this,batteryListView)
--- app/src/main/java/com/example/androidDeviceDetails/adapters/BatteryListAdapter.kt
+++ app/src/main/java/com/example/androidDeviceDetails/adapters/BatteryListAdapter.kt
@@ -2,7 +2,6 @@ package com.example.androidDeviceDetails.adapters
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.pm.PackageManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
