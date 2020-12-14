cbfefe3529be9fda0f6ca473907f876b95365a32
Kenneth Godwin <kennethgodwin06@gmail.com>
2020-12-14 06:32:55 -0500

    --Implemented popup datePicker

--- app/src/main/java/com/example/androidDeviceDetails/AppInfoActivity.kt
+++ app/src/main/java/com/example/androidDeviceDetails/AppInfoActivity.kt
@@ -1,13 +1,13 @@
 package com.example.androidDeviceDetails
 
 import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.androidDeviceDetails.adapters.AppInfoListAdapter
-import com.example.androidDeviceDetails.adapters.BatteryListAdapter
 import com.example.androidDeviceDetails.databinding.ActivityAppInfoBinding
 import com.example.androidDeviceDetails.managers.AppStateCooker
 import com.example.androidDeviceDetails.models.AppInfoCookedData
@@ -34,37 +34,60 @@ class AppInfoActivity : AppCompatActivity() {
 
         val binding: ActivityAppInfoBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_app_info)
-        var appList: List<AppInfoCookedData>
-        val today: Calendar = Calendar.getInstance()
-        binding.datePicker.init(
-            today.get(Calendar.YEAR), today.get(Calendar.MONTH),
-            today.get(Calendar.DAY_OF_MONTH)
-        )
-        { _, year, month, day ->
-            today.set(year, month, day)
-            today[Calendar.HOUR_OF_DAY] = 0
-            today[Calendar.MINUTE] = 0
-            today[Calendar.SECOND] = 0
-            val startTime = today.timeInMillis
-            val endTime = startTime + (((((23 * 60) + 59) * 60) + 59) * 1000)
-            var swapText = ""
-            GlobalScope.launch(Dispatchers.IO) {
-                appList = AppStateCooker.createInstance()
-                    .getAppsBetween(startTime, endTime, applicationContext)
-                val db = RoomDB.getDatabase(applicationContext)!!
-                for (app in appList) {
-                    app.packageName = db.appsDao().getPackageByID(app.appId)
-                }
-                appList = appList.sortedBy { it.appName }
-                binding.appInfoListView.post {
-                    binding.appInfoListView.adapter =
-                        AppInfoListAdapter(applicationContext, R.layout.appinfo_tile, appList)
-                }
 
 
-            }
+        var day :Int
+        var month :Int
+        var year :Int
+
+
+
+
+        binding.button.setOnClickListener {
+            var appList: List<AppInfoCookedData>
+            val calendar: Calendar = Calendar.getInstance()
+            day = calendar.get(Calendar.DAY_OF_MONTH)
+            month = calendar.get(Calendar.MONTH)
+            year = calendar.get(Calendar.YEAR)
+            @Suppress("RedundantSamConstructor") val datePickerDialog =
+                DatePickerDialog(
+                    this@AppInfoActivity,
+                    DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                        calendar.set(year, month, dayOfMonth)
+                        calendar[Calendar.HOUR_OF_DAY] = 0
+                        calendar[Calendar.MINUTE] = 0
+                        calendar[Calendar.SECOND] = 0
+                        val startTime = calendar.timeInMillis
+                        val endTime = startTime + (((((23 * 60) + 59) * 60) + 59) * 1000)
+                        GlobalScope.launch(Dispatchers.IO) {
+                            appList = AppStateCooker.createInstance()
+                                .getAppsBetween(startTime, endTime, applicationContext)
+                            val db = RoomDB.getDatabase(applicationContext)!!
+                            for (app in appList) {
+                                app.packageName = db.appsDao().getPackageByID(app.appId)
+                            }
+                            appList = appList.sortedBy { it.appName }
+                            binding.appInfoListView.post {
+                                binding.appInfoListView.adapter = null
+                                binding.appInfoListView.adapter =
+                                    AppInfoListAdapter(
+                                        applicationContext,
+                                        R.layout.appinfo_tile,
+                                        appList
+                                    )
+                                val date = "$dayOfMonth / ${month+1} / $year"
+                                binding.dateView.text = date
+                            }
+                        }
+
+                    },
+                    year,
+                    month,
+                    day
+                )
+            datePickerDialog.show()
         }
+
     }
-
-
 }
+
