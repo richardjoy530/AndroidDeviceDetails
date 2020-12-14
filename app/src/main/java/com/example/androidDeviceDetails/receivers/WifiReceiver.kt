88b4ade8ed62b60b444ee476d70c92218dcd0506
neena38 <neena.mec@gmail.com>
2020-12-14 11:48:14 +0530

    modified wifi entry table

--- app/src/main/java/com/example/androidDeviceDetails/receivers/WifiReceiver.kt
+++ app/src/main/java/com/example/androidDeviceDetails/receivers/WifiReceiver.kt
@@ -3,43 +3,38 @@ package com.example.androidDeviceDetails.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 import android.net.wifi.WifiManager
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.example.androidDeviceDetails.DeviceDetailsApplication
-import com.example.androidDeviceDetails.base.BaseCollector
 import com.example.androidDeviceDetails.models.RoomDB
 import com.example.androidDeviceDetails.models.WifiRaw
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
-class WifiReceiver : BaseCollector() {
+internal class WifiReceiver(private val context: Context) : BroadcastReceiver() {
     private var signalDB = RoomDB.getDatabase()!!
 
-    object broadcastReceiver : BroadcastReceiver(){
-        override fun onReceive(context: Context, intent: Intent) {
-            val wifiRaw: WifiRaw
-            var level = 0
-            var strength = 0
+    override fun onReceive(context: Context, intent: Intent) {
+        val wifiRaw: WifiRaw
+        var level = 0
+        var strength = 0
+        var frequency = 0
+        var linkSpeed = 0
 
-            val wifiManager =
-                context.applicationContext.getSystemService(AppCompatActivity.WIFI_SERVICE) as WifiManager
-            strength = wifiManager.connectionInfo.rssi
-            //could add link speed, frequency
-            //Log.d("details", "onReceive: ${wifiManager.connectionInfo.ipAddress}   ${wifiManager.connectionInfo.linkSpeed} ${wifiManager.connectionInfo.frequency} ${wifiManager.connectionInfo.macAddress}")
-            level = WifiReceiver().getLevel(strength)
-            wifiRaw = WifiRaw(
-                System.currentTimeMillis(), strength, level
-            )
+        val wifiManager =
+            context.applicationContext.getSystemService(AppCompatActivity.WIFI_SERVICE) as WifiManager
+        strength = wifiManager.connectionInfo.rssi
+        frequency = wifiManager.connectionInfo.linkSpeed
+        linkSpeed=wifiManager.connectionInfo.frequency
+        level = getLevel(strength)
 
-            Log.d("wifi", "onReceive: $strength,$level")
-            /*GlobalScope.launch {
-                signalDB.wifiDao().insertAll(wifiRaw)
-            }*/
+        wifiRaw = WifiRaw(
+            System.currentTimeMillis(), strength, level,frequency,linkSpeed
+        )
+        Log.d("wifi", "onReceive: $strength,$level,$frequency,$linkSpeed")
+        GlobalScope.launch {
+            signalDB.wifiDao().insertAll(wifiRaw)
         }
-    }
-
-    init {
-        start()
     }
 
     private fun getLevel(strength: Int): Int {
@@ -49,15 +44,5 @@ class WifiReceiver : BaseCollector() {
             strength > -70 -> 1
             else -> 0
         }
-    }
-
-    override fun start() {
-        val intentWifi = IntentFilter()
-        intentWifi.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
-        DeviceDetailsApplication.instance.registerReceiver(broadcastReceiver, intentWifi)
-    }
-
-    override fun stop() {
-        DeviceDetailsApplication.instance.unregisterReceiver(broadcastReceiver)
     }
 }
