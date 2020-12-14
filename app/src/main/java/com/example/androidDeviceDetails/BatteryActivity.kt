933df0efc4bd62af4df76e591fb1853ada96da16
Richard Joy <richardjoy530@gmail.com>
2020-12-14 22:38:29 +0530

    Minor refactoring done

--- app/src/main/java/com/example/androidDeviceDetails/BatteryActivity.kt
+++ app/src/main/java/com/example/androidDeviceDetails/BatteryActivity.kt
@@ -5,15 +5,13 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
-import android.util.Log
 import android.view.View
-import android.widget.ImageButton
-import android.widget.LinearLayout
-import android.widget.ListView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.databinding.DataBindingUtil
 import com.example.androidDeviceDetails.adapters.BatteryListAdapter
+import com.example.androidDeviceDetails.databinding.ActivityBatteryBinding
 import com.example.androidDeviceDetails.managers.AppBatteryUsageManager
 import com.example.androidDeviceDetails.utils.Utils
 import java.util.*
@@ -21,115 +19,63 @@ import java.util.*
 
 class BatteryActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var calendar: Calendar
-    private lateinit var totalTextView: TextView
-    private lateinit var todayTextView: TextView
-    private lateinit var batteryListView: ListView
+    private lateinit var batteryBinding: ActivityBatteryBinding
 
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_battery)
-        batteryListView = findViewById(R.id.batteryListView)
-        totalTextView = findViewById(R.id.total)
-        todayTextView = findViewById(R.id.today)
-
-        val previousDayButton = findViewById<ImageButton>(R.id.leftArrow)
-        val nextDayButton = findViewById<ImageButton>(R.id.rightArrow)
-        val timePickerLayout = findViewById<LinearLayout>(R.id.battery_date_picker)
-        timePickerLayout.isVisible = false
-        previousDayButton.setOnClickListener(this)
-        nextDayButton.setOnClickListener(this)
-        calendar = Calendar.getInstance()
-        calendar[Calendar.HOUR_OF_DAY] = 0
-        calendar[Calendar.MINUTE] = 0
-        calendar[Calendar.SECOND] = 0
-//        val items = arrayListOf<AppEntry>()
-//        items.add(AppEntry("com.whatsapp", 6))
-//        items.add(AppEntry("com.example.Broadband", 4))
-//        items.add(AppEntry("com.android.packageinstaller", 4))
-//        items.add(AppEntry("com.microsoft.teams", 4))
-//        items.add(AppEntry("com.google.android.apps.photos", 4))
-//        batteryListView.adapter = BatteryListAdapter(this, R.layout.battery_tile, items)
-
-        AppBatteryUsageManager().cookBatteryData(
-            this,
-            batteryListView,
-            totalTextView,
-            calendar.timeInMillis,
-            calendar.timeInMillis + 24 * 60 * 60 * 1000
-        )
-        batteryListView.setOnItemClickListener { parent, _, position, _ ->
+        batteryBinding = DataBindingUtil.setContentView(this, R.layout.activity_battery)
+        batteryBinding.batteryDatePicker.isVisible = false
+        batteryBinding.leftArrow.setOnClickListener(this)
+        batteryBinding.rightArrow.setOnClickListener(this)
+        batteryBinding.description.setOnClickListener(this)
+        setCalender(0, true)
+        batteryBinding.batteryListView.setOnItemClickListener { parent, _, position, _ ->
             val adapter = parent.adapter as BatteryListAdapter
             val item = adapter.getItem(position)
-            Log.d("TAG", "onCreate: $position")
             val infoIntent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
             infoIntent.addCategory(Intent.CATEGORY_DEFAULT)
             infoIntent.data = Uri.parse("package:${item?.packageId}")
             startActivity(infoIntent)
-        }
-
-        findViewById<TextView>(R.id.description).setOnClickListener {
-            it as TextView
-            if (it.text == "Till today") {
-                it.text = "Day wise"
-                timePickerLayout.isVisible = true
-            } else {
-                it.text = "Till today"
-                timePickerLayout.isVisible = false
-            }
-            calendar = Calendar.getInstance()
-            calendar[Calendar.HOUR_OF_DAY] = 0
-            calendar[Calendar.MINUTE] = 0
-            calendar[Calendar.SECOND] = 0
-            todayTextView.text =
-                "${Utils.getWeek(calendar.get(Calendar.DAY_OF_WEEK))}, ${calendar.get(Calendar.DAY_OF_MONTH)} ${
-                    Utils.getMonth(calendar.get(Calendar.MONTH))
-                }"
-            AppBatteryUsageManager().cookBatteryData(
-                this,
-                batteryListView,
-                totalTextView,
-                calendar.timeInMillis,
-                calendar.timeInMillis + 24 * 60 * 60 * 1000
-            )
         }
     }
 
     @SuppressLint("SetTextI18n")
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.leftArrow -> {
-                Log.d("TAG", "onClick: left")
-                calendar.add(Calendar.DAY_OF_MONTH, -1)
-                todayTextView.text =
-                    "${Utils.getWeek(calendar.get(Calendar.DAY_OF_WEEK))}, ${calendar.get(Calendar.DAY_OF_MONTH)} ${
-                        Utils.getMonth(calendar.get(Calendar.MONTH))
-                    }"
-                AppBatteryUsageManager().cookBatteryData(
-                    this,
-                    batteryListView,
-                    totalTextView,
-                    calendar.timeInMillis,
-                    calendar.timeInMillis + 24 * 60 * 60 * 1000
-                )
-
-            }
-            R.id.rightArrow -> {
-                Log.d("TAG", "onClick: right")
-                calendar.add(Calendar.DAY_OF_MONTH, 1)
-                todayTextView.text =
-                    "${Utils.getWeek(calendar.get(Calendar.DAY_OF_WEEK))}, ${calendar.get(Calendar.DAY_OF_MONTH)} ${
-                        Utils.getMonth(calendar.get(Calendar.MONTH))
-                    }"
-                AppBatteryUsageManager().cookBatteryData(
-                    this,
-                    batteryListView,
-                    totalTextView,
-                    calendar.timeInMillis,
-                    calendar.timeInMillis + 24 * 60 * 60 * 1000
-                )
+            R.id.leftArrow -> setCalender(-1)
+            R.id.rightArrow -> setCalender(1)
+            R.id.description -> {
+                v as TextView
+                if (v.text == "Till today") {
+                    v.text = "Day wise"
+                    batteryBinding.batteryDatePicker.isVisible = true
+                } else {
+                    v.text = "Till today"
+                    batteryBinding.batteryDatePicker.isVisible = false
+                }
+                setCalender(0, true)
             }
         }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setCalender(offset: Int = 0, reset: Boolean = false) {
+        if (reset) calendar = Calendar.getInstance()
+        calendar[Calendar.HOUR_OF_DAY] = 0
+        calendar[Calendar.MINUTE] = 0
+        calendar[Calendar.SECOND] = 0
+        calendar.add(Calendar.DAY_OF_MONTH, offset)
+        batteryBinding.today.text =
+            "${Utils.getWeek(calendar.get(Calendar.DAY_OF_WEEK))}, ${calendar.get(Calendar.DAY_OF_MONTH)} ${
+                Utils.getMonth(calendar.get(Calendar.MONTH))
+            }"
+        AppBatteryUsageManager().cookBatteryData(
+            this,
+            batteryBinding.batteryListView,
+            batteryBinding.total,
+            calendar.timeInMillis,
+            calendar.timeInMillis + 24 * 60 * 60 * 1000
+        )
     }
 }
 
