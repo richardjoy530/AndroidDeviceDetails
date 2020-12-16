d54909b31f3897662c7b938e19b8a18911e153a4
keapen <kurieneapenk@gmail.com>
2020-12-16 18:33:46 +0530

    banner indication for event type implemented

--- app/src/main/java/com/example/androidDeviceDetails/adapters/AppInfoListAdapter.kt
+++ app/src/main/java/com/example/androidDeviceDetails/adapters/AppInfoListAdapter.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.example.androidDeviceDetails.R
 import com.example.androidDeviceDetails.models.AppInfoCookedData
 import com.example.androidDeviceDetails.utils.Utils
@@ -26,12 +27,20 @@ class AppInfoListAdapter(
         val versionCodeTextView = view.findViewById<TextView>(R.id.appVersionCode)
         val eventTypeTextView = view.findViewById<TextView>(R.id.appEvent)
         val appIconView = view.findViewById<ImageView>(R.id.appIcon)
+        val eventBadge = view.findViewById<ImageView>(R.id.event_badge)
 
         appNameView.text = items[position].appName
         versionCodeTextView.text = "Version Code : " + items[position].versionCode.toString()
         eventTypeTextView.text = " | Event : " + items[position].eventType.toString()
         appIconView.setImageDrawable(Utils.getApplicationIcon(items[position].packageName))
-
+        val color = when (items[position].eventType.ordinal) {
+            0 -> R.color.teal_700
+            1 -> R.color.purple_500
+            2 -> R.color.pink
+            3 -> R.color.mat_yellow
+            else -> R.color.teal_700
+        }
+        eventBadge.setColorFilter(ContextCompat.getColor(context, color), android.graphics.PorterDuff.Mode.MULTIPLY)
         return view
     }
 }
