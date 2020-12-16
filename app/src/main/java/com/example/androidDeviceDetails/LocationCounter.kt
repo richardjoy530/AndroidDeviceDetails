1cf3869a4e03d595241ee3194c34914180daa741
isham <mail.to.isham@gmail.com>
2020-12-16 17:01:23 +0530

    graph added

--- app/src/main/java/com/example/androidDeviceDetails/LocationCounter.kt
+++ app/src/main/java/com/example/androidDeviceDetails/LocationCounter.kt
@@ -1,7 +1,9 @@
 package com.example.androidDeviceDetails
 
 import com.example.androidDeviceDetails.models.CountModel
+import com.example.androidDeviceDetails.models.LocationModel
 import com.example.androidDeviceDetails.models.RoomDB
+import com.fonfon.kgeohash.GeoHash
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -9,22 +11,22 @@ class LocationCounter {
     private val geoHashLength: Int = 6
     private var locationDatabase = RoomDB.getDatabase()
 
-    fun countLocation2(): List<CountModel> {
-//        val locationHashList= emptyList<String>().toMutableList()
-//        var prevLocationHash = ""
-//        for (location in locationList) {
-//            val newLocationHash = GeoHash(
-//                location.latitude!!.toDouble(),
-//                location.longitude!!.toDouble(),
-//                geoHashLength
-//            ).toString()
-//            if (prevLocationHash != newLocationHash) {
-//                prevLocationHash = newLocationHash
-//                locationHashList.add(newLocationHash)
-//            }
-//        }
-//        return locationHashList.groupingBy { it }.eachCount()
-        return locationDatabase!!.locationDao().countHash()
+    fun countLocation(locationList : List<LocationModel>): Map<String, Int> {
+        val locationHashList= emptyList<String>().toMutableList()
+        var prevLocationHash = ""
+        for (location in locationList) {
+            val newLocationHash = GeoHash(
+                location.latitude!!.toDouble(),
+                location.longitude!!.toDouble(),
+                geoHashLength
+            ).toString()
+            if (prevLocationHash != newLocationHash) {
+                prevLocationHash = newLocationHash
+                locationHashList.add(newLocationHash)
+            }
+        }
+        return locationHashList.groupingBy { it }.eachCount()
+//        return locationDatabase!!.locationDao().countHash()
 
     }
 
--- app/src/main/java/com/example/androidDeviceDetails/ui/LocationActivity.kt
+++ app/src/main/java/com/example/androidDeviceDetails/ui/LocationActivity.kt
@@ -1,19 +1,14 @@
 package com.example.androidDeviceDetails.ui
 
 import android.Manifest
-import android.annotation.SuppressLint
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
 import android.util.Log
+import android.view.Gravity
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import android.widget.TableLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidDeviceDetails.LocationCounter
 import com.example.androidDeviceDetails.R
@@ -21,199 +16,123 @@ import com.example.androidDeviceDetails.models.CountModel
 import com.example.androidDeviceDetails.models.LocationModel
 import com.example.androidDeviceDetails.models.RoomDB
 import com.fonfon.kgeohash.GeoHash
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.XAxis.XAxisPosition.BOTTOM
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.ValueFormatter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.util.*
 
+
 private const val PERMISSION_REQUEST = 10
 
 class LocationActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var locationManager: LocationManager
     private lateinit var locationCounter: LocationCounter
-    private var hasGps = false
-    private var hasNetwork = false
-    private var locationGps: Location? = null
-    private var locationNetwork: Location? = null
+    private lateinit var locationDatabase: RoomDB
+
     private lateinit var geoHashView: TextView
     private lateinit var countLocation: Button
-    private lateinit var btnGetLocation: Button
+    private lateinit var tableView: TableLayout
+    private lateinit var barChart: BarChart
 
     private var permissions = arrayOf(
         Manifest.permission.ACCESS_FINE_LOCATION,
         Manifest.permission.ACCESS_COARSE_LOCATION
     )
-    private lateinit var locationDatabase: RoomDB
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_location)
         geoHashView = findViewById(R.id.geoHashView)
         countLocation = findViewById(R.id.countLocation)
-        btnGetLocation = findViewById(R.id.btnGetLocation)
-        btnGetLocation.setOnClickListener(this)
+        tableView = findViewById(R.id.tableView)
+        barChart = findViewById(R.id.barChart)
         countLocation.setOnClickListener(this)
         locationCounter = LocationCounter()
         locationDatabase = RoomDB.getDatabase()!!
-        disableView()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkPermission(permissions)) {
-                enableView()
-            } else {
-                requestPermissions(permissions, PERMISSION_REQUEST)
-            }
-        } else {
-            enableView()
-        }
-    }
-
-    private fun disableView() {
-        val btnGetLocation: Button = findViewById(R.id.btnGetLocation)
-        btnGetLocation.isEnabled = false
-        btnGetLocation.alpha = 0.5F
-    }
-
-    private fun enableView() {
-        val btnGetLocation: Button = findViewById(R.id.btnGetLocation)
-        btnGetLocation.isEnabled = true
-        btnGetLocation.alpha = 1F
-        Toast.makeText(this, "Done", Toast.LENGTH_SHORT).show()
-    }
-
-    fun insert(location: Location?, tag: String) {
-        val locationView: TextView = findViewById(R.id.textView)
-        locationView.append("$tag \nLatitude: " + location!!.latitude + "\nLongitude: " + location.longitude + "\nTimestamp: " + Calendar.getInstance().time.toString() + "\n\n")
-        val geoHash = GeoHash(
-            location.latitude,
-            location.longitude,
-            6
-        ).toString()
-        Log.d("Time", Calendar.getInstance().time.toString())
-        Log.d("CodeAndroidLocation", "Latitude : " + location.latitude)
-        Log.d("CodeAndroidLocation", "Longitude : " + location.longitude)
-        Log.d("CodeAndroidLocation", "GeoHash : $geoHash")
-        val locationObj = LocationModel(
-            0, location.latitude, location.longitude, geoHash,
-            Calendar.getInstance().time.toString()
-        )
-        GlobalScope.launch {
-            locationDatabase.locationDao().insertLocation(locationObj)
-        }
-        Toast.makeText(applicationContext, "Added to Database", Toast.LENGTH_LONG).show()
     }
 
     private suspend fun getData(): List<LocationModel> = withContext(Dispatchers.IO) {
         return@withContext locationDatabase.locationDao().readAll()
     }
 
-    private suspend fun countLocation(): List<CountModel> = withContext(Dispatchers.IO) {
-        return@withContext locationDatabase.locationDao().countHash()
+    private fun getTextView(text: String): TextView {
+        val textView = TextView(this)
+        val tlp = TableRow.LayoutParams(
+            TableRow.LayoutParams.MATCH_PARENT,
+            TableRow.LayoutParams.WRAP_CONTENT
+        )
+        tlp.weight = 0.5F
+        textView.layoutParams = tlp
+        textView.gravity = Gravity.CENTER
+        textView.text = text
+        return textView
     }
 
-    @SuppressLint("MissingPermission")
-    private fun getLocation() {
-        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
-        hasGps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-        hasNetwork = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-        if (hasGps || hasNetwork) {
-            if (hasGps) {
-                Log.d("CodeAndroidLocation", "hasGps")
-                locationManager.requestLocationUpdates(
-                    LocationManager.GPS_PROVIDER,
-                    5000,
-                    0F
-                ) { location ->
-                    Log.d("CodeAndroidLocation", "gpsLocation not null")
-                    locationGps = location
-                    insert(locationGps, "GPS")
-                }
-            }
-            if (hasNetwork) {
-                Log.d("CodeAndroidLocation", "hasNetworkGps")
-                locationManager.requestLocationUpdates(
-                    LocationManager.NETWORK_PROVIDER,
-                    5000,
-                    0F
-                ) { location ->
-                    Log.d("CodeAndroidLocation", "networkLocation not null")
-                    locationNetwork = location
-                    insert(locationNetwork, "Network")
-                }
-            }
-            if (locationGps != null && locationNetwork != null) {
-                Log.d("CodeAndroidLocation", "has both")
-                if (locationGps!!.accuracy > locationNetwork!!.accuracy) {
-                    insert(locationNetwork, "mNetwork")
-                } else {
-                    insert(locationGps, "mGPS")
-                }
-            }
-            GlobalScope.launch {
-                val result = getData()
-                if (result.isNotEmpty()) {
-                    for (i in result.indices) {
-                        Log.d("Latitude", result[i].latitude.toString())
-                        Log.d("Longitude", result[i].longitude.toString())
-                        Log.d("Time", result[i].time.toString())
-                    }
-                }
-            }
-        } else {
-            startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+    private fun buildGraph(res: Map<String, Int>) {
+        val entries: MutableList<BarEntry> = emptyList<BarEntry>().toMutableList()
+        val labels = emptyArray<String>().toMutableList()
+        var index = 0f
+        for ((k, v) in res){
+            entries.add(BarEntry(index, v.toFloat()))
+            labels.add(k)
+            index+=1
         }
-    }
-
-    private fun checkPermission(permissionArray: Array<String>): Boolean {
-        var allSuccess = true
-        for (i in permissionArray.indices) {
-            if (checkCallingOrSelfPermission(permissionArray[i]) == PackageManager.PERMISSION_DENIED)
-                allSuccess = false
-        }
-        return allSuccess
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == PERMISSION_REQUEST) {
-            var allSuccess = true
-            for (i in permissions.indices) {
-                if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
-                    allSuccess = false
-                    val requestAgain =
-                        Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && shouldShowRequestPermissionRationale(
-                            permissions[i]
-                        )
-                    if (requestAgain) {
-                        Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
-                    } else {
-                        Toast.makeText(
-                            this,
-                            "Go to settings and enable the permission",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
+        val set = BarDataSet(entries,"Location Count")
+        val data = BarData(set)
+        data.barWidth = 0.9f // set custom bar width
+        barChart.setDrawGridBackground(false)
+        barChart.data = data
+        barChart.description = Description()
+        barChart.setDrawGridBackground(false)
+        barChart.setFitBars(true) // make the x-axis fit exactly all bars
+        val formatter: ValueFormatter = object : ValueFormatter() {
+            override fun getAxisLabel(value: Float, axis: AxisBase): String {
+                return labels[value.toInt()]
             }
-            if (allSuccess)
-                enableView()
+        }
+        val xAxis: XAxis = barChart.xAxis
+        xAxis.granularity = 1f // minimum axis-step (interval) is 1
+        xAxis.valueFormatter = formatter
+        xAxis.position= BOTTOM
+        barChart.invalidate() // refresh
+    }
+
+    private fun buildTable(countLocation: Map<String, Int>) {
+        tableView.removeAllViews()
+        for ((k, v) in countLocation) {
+            val row = TableRow(this)
+            val lp = TableLayout.LayoutParams(
+                TableLayout.LayoutParams.MATCH_PARENT,
+                TableLayout.LayoutParams.WRAP_CONTENT
+            )
+            row.layoutParams = lp
+            val geoHashTextView = getTextView(k)
+            val countTextView = getTextView(v.toString())
+            row.addView(geoHashTextView)
+            row.addView(countTextView)
+            tableView.addView(row)
         }
     }
 
     override fun onClick(v: View?) {
         when (v!!.id) {
-            R.id.btnGetLocation -> getLocation()
             R.id.countLocation -> {
                 GlobalScope.launch {
-                    val res = countLocation()
+                    val res = getData()
                     geoHashView.post {
-                        geoHashView.text = ""
-                        for (hash in res)
-                            geoHashView.append("GeoHash:" + hash.geoHash + " = " + hash.count + "\n")
+                        buildGraph(locationCounter.countLocation(res))
+                        buildTable(locationCounter.countLocation(res))
+                        geoHashView.text = locationCounter.countLocation(res).toString()
                     }
                 }
             }
