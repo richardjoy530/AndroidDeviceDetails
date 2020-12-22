0526ef5aeaa3d13020185e8c3b0531dbfce470fc
neena38 <neena.mec@gmail.com>
2020-12-22 23:28:57 +0530

    added user interface via fragments

--- app/src/main/java/com/example/androidDeviceDetails/NavActivity.kt
+++ app/src/main/java/com/example/androidDeviceDetails/NavActivity.kt
@@ -1,7 +1,9 @@
 package com.example.androidDeviceDetails
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -10,14 +12,46 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class NavActivity : AppCompatActivity() {
+    private var wifiFragment = WifiFragment()
+    private var cellularFragment = CellularFragment()
+    private lateinit var active: Fragment
+    private var fragmentManager = supportFragmentManager
+    lateinit var bottomNavigation: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        active=cellularFragment
         setContentView(R.layout.activity_nav)
+        bottomNavigation = findViewById(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.wifiStrength,
+                R.id.cellularStrength
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNavigation.setupWithNavController(navController)
 
-        var BottomNav=findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        val navController=findNavController(R.id.fragment)
-        val appBarConfiguration= AppBarConfiguration(setOf(R.id.wifiStrength,R.id.cellularStrength))
-        setupActionBarWithNavController(navController,appBarConfiguration)
-        BottomNav.setupWithNavController(navController)
+        fragmentManager.beginTransaction().add(R.id.fragment, wifiFragment, "2").hide(wifiFragment)
+            .commit();
+        fragmentManager.beginTransaction().add(R.id.fragment, cellularFragment, "1").commit();
+
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.cellularStrength -> {
+                    fragmentManager.beginTransaction().hide(active).show(cellularFragment).commit()
+                    active = cellularFragment
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.wifiStrength -> {
+                    fragmentManager.beginTransaction().hide(active).show(wifiFragment).commit()
+                    active = wifiFragment
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+            }
+            false
+        }
     }
 }
--- app/src/main/java/com/example/androidDeviceDetails/WifiFragment.kt
+++ app/src/main/java/com/example/androidDeviceDetails/WifiFragment.kt
@@ -1,18 +1,54 @@
 package com.example.androidDeviceDetails
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.example.androidDeviceDetails.models.RoomDB
+import com.example.androidDeviceDetails.models.WifiRaw
+import de.nitri.gauge.Gauge
 
 class WifiFragment : Fragment() {
+    private var db = RoomDB.getDatabase()!!
+    private var wifiStrength: Int = -80
+    private var linkspeed: Int = 0
+    private lateinit var linkspeedText : TextView
+    private lateinit var strength: TextView
+    lateinit private var gauge: Gauge
+    lateinit var view: ViewGroup
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_wifi, container, false)
+    ): View {
+        view = inflater.inflate(R.layout.fragment_wifi_strength, container, false) as ViewGroup
+        gauge = view.findViewById(R.id.wifiGauge)
+        strength=view.findViewById(R.id.wifiStrengthText)
+        linkspeedText=view.findViewById(R.id.linkspeed)
+        updateGauge()
+        db.wifiDao().getLastLive().observe(viewLifecycleOwner) {
+            updateWifiGauge(it)
+        }
+        return view
     }
+
+    private fun updateGauge() {
+        gauge.moveToValue(wifiStrength.toFloat())
+        gauge.setLowerText(linkspeed.toString())
+        gauge.setUpperText(wifiStrength.toString())
+        strength.text=wifiStrength.toString()
+        linkspeedText.text=linkspeed.toString()
+    }
+
+    private fun updateWifiGauge(wifiRaw: WifiRaw) {
+        Log.d("psy", "updateWifiGauge: ")
+        wifiStrength = wifiRaw.strength!!
+        linkspeed = wifiRaw.linkSpeed!!
+        updateGauge()
+    }
+
 
 }
