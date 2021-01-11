a6c7aa0d29aed7b374ec4aa33564aa5d26e39394
Richard Joy <richardjoy530@gmail.com>
2021-01-11 13:30:09 +0530

    Renamed Classes and bug fix

--- app/src/main/java/com/example/androidDeviceDetails/controller/ApplicationController.kt
+++ app/src/main/java/com/example/androidDeviceDetails/controller/ApplicationController.kt
@@ -2,33 +2,31 @@ package com.example.androidDeviceDetails.controller
 
 import com.example.androidDeviceDetails.DeviceDetailsApplication
 import com.example.androidDeviceDetails.base.BaseCollector
+import com.example.androidDeviceDetails.collectors.BatteryCollector
+import com.example.androidDeviceDetails.collectors.NetworkUsageCollector
+import com.example.androidDeviceDetails.collectors.SignalChangeListener
 import com.example.androidDeviceDetails.managers.AppEventCollector
-import com.example.androidDeviceDetails.managers.NetworkUsageCollector
-import com.example.androidDeviceDetails.managers.SignalChangeListener
 import com.example.androidDeviceDetails.receivers.AppInfoReceiver
-import com.example.androidDeviceDetails.receivers.BatteryReceiver
 import com.example.androidDeviceDetails.receivers.WifiReceiver
 import java.util.*
 
 class ApplicationController {
     lateinit var timer: Timer
-    var instanceMap : MutableMap<String, BaseCollector>
-
-    init{
-        instanceMap = mutableMapOf("BatteryReceiver" to BatteryReceiver(),
-            "WifiReceiver" to WifiReceiver(),
-            "AppStateReceiver" to AppInfoReceiver(),
-            "AppEventCollector" to AppEventCollector(DeviceDetailsApplication.instance),
-            "SignalChangeListener" to SignalChangeListener(DeviceDetailsApplication.instance),
-            "NetworkUsageCollector" to NetworkUsageCollector(DeviceDetailsApplication.instance))
-    }
+    var instanceMap: MutableMap<String, BaseCollector> = mutableMapOf(
+        "BatteryReceiver" to BatteryCollector(),
+        "WifiReceiver" to WifiReceiver(),
+        "AppStateReceiver" to AppInfoReceiver(),
+        "AppEventCollector" to AppEventCollector(DeviceDetailsApplication.instance),
+        "SignalChangeListener" to SignalChangeListener(DeviceDetailsApplication.instance),
+        "NetworkUsageCollector" to NetworkUsageCollector(DeviceDetailsApplication.instance)
+    )
 
     fun runTimer(intervalInMinuets: Long) {
         timer = Timer()
         timer.scheduleAtFixedRate(
             object : TimerTask() {
                 override fun run() {
-                    for(collector in instanceMap.values) {
+                    for (collector in instanceMap.values) {
                         collector.collect()
                     }
                 }
