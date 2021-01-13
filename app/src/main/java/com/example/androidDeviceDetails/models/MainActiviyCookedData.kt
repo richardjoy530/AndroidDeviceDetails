d76c1d938e54bb0cf00f59a978947c61f4515e33
Kenneth Godwin <kennethgodwin06@gmail.com>
2021-01-13 01:11:55 -0500

    --Removed unwanted function call from Network and Battery Activities.

--- app/src/main/java/com/example/androidDeviceDetails/models/MainActiviyCookedData.kt
+++ app/src/main/java/com/example/androidDeviceDetails/models/MainActiviyCookedData.kt
@@ -2,8 +2,10 @@ package com.example.androidDeviceDetails.models
 
 import com.example.androidDeviceDetails.models.appInfoModels.AppInfoCookedData
 import com.example.androidDeviceDetails.models.batteryModels.BatteryAppEntry
+import com.example.androidDeviceDetails.models.networkUsageModels.DeviceNetworkUsageEntity
 
 data class MainActiviyCookedData(
     var appInfo:AppInfoCookedData,
-    var batteryData: BatteryAppEntry
+    var batteryData: BatteryAppEntry,
+    var deviceNetworkUsage: DeviceNetworkUsageEntity
 )
