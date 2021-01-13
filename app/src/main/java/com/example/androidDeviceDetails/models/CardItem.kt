95585753756dbb221ec95f62d798217c0e648f9e
keapen <kurieneapenk@gmail.com>
2021-01-13 13:52:46 +0530

    --partial update for MainActivityViewModel.kt

--- app/src/main/java/com/example/androidDeviceDetails/models/CardItem.kt
+++ app/src/main/java/com/example/androidDeviceDetails/models/CardItem.kt
@@ -2,5 +2,15 @@ package com.example.androidDeviceDetails.models
 
 data class CardItem (
     var image: Int = 0,
-    var name: String? = null
+    var title: String? = null,
+    var layoutType : Int,
+    var mainValue :  Int,
+    var superscript : String,
+    var subscript : String,
+    var progressbarFirst : Int,
+    var progressbarSecond : Int,
+    var label1 : String,
+    var label2 : String,
+    var label1Value : String,
+    var label2Value : String
 )
