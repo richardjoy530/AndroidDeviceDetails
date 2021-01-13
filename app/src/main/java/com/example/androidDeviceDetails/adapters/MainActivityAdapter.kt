0c929ebb37d951788c707d00a57850f4af0b50e1
keapen <kurieneapenk@gmail.com>
2021-01-13 16:15:58 +0530

    --implemented new card UI --implemented themes in new card UI

--- app/src/main/java/com/example/androidDeviceDetails/adapters/MainActivityAdapter.kt
+++ app/src/main/java/com/example/androidDeviceDetails/adapters/MainActivityAdapter.kt
@@ -3,14 +3,18 @@ package com.example.androidDeviceDetails.adapters
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidDeviceDetails.R
 import com.example.androidDeviceDetails.models.CardItem
+import com.example.androidDeviceDetails.models.LayoutType
 
 
 class MainActivityAdapter(var context: Context, private var arrayList: ArrayList<CardItem>) :
@@ -27,11 +31,24 @@ class MainActivityAdapter(var context: Context, private var arrayList: ArrayList
     override fun onBindViewHolder(ViewHolder: ViewHolder, position: Int) {
         ViewHolder.title.text = arrayList[position].title
         ViewHolder.icon.setImageResource(arrayList[position].image)
-        ViewHolder.title.setTextColor(R.attr.batteryTitle)
-        if(arrayList[position].layoutType == 1){
-
+        if(arrayList[position].layoutType == LayoutType.PROGRESSBAR_LAYOUT.ordinal){
+            ViewHolder.usingSingleValue.visibility = GONE
+            ViewHolder.usingProgressBar.visibility = VISIBLE
+            ViewHolder.progressbarFirst.progress = arrayList[position].progressbarFirst
+            ViewHolder.progressbarSecond.progress = arrayList[position].progressbarSecond
+            ViewHolder.label1.text = arrayList[position].label1
+            ViewHolder.label2.text = arrayList[position].label2
+            ViewHolder.label1Value.text = arrayList[position].label1Value
+            ViewHolder.label2Value.text = arrayList[position].label2Value
         }
-//        ViewHolder.cardView.setCardBackgroundColor(R.attr.mainButtonBg)
+        if(arrayList[position].layoutType == LayoutType.SINGLE_VALUE_LAYOUT.ordinal){
+            ViewHolder.usingProgressBar.visibility = GONE
+            ViewHolder.usingSingleValue.visibility = VISIBLE
+            ViewHolder.mainValue.text = arrayList[position].mainValue.toString()
+            ViewHolder.superscript.text = arrayList[position].superscript
+            ViewHolder.subscript.text = arrayList[position].subscript
+        }
+
     }
 
     override fun getItemCount(): Int {
@@ -45,7 +62,7 @@ class MainActivityAdapter(var context: Context, private var arrayList: ArrayList
         var mainValue : TextView = itemView.findViewById(R.id.mainValue)
         var superscript : TextView = itemView.findViewById(R.id.superscript)
         var subscript : TextView = itemView.findViewById(R.id.subscript)
-        var usingProgressBar : ConstraintLayout = itemView.findViewById(R.id.usingProgressBar)
+        var usingProgressBar : LinearLayout = itemView.findViewById(R.id.usingProgressBar)
         var progressbarFirst : ProgressBar = itemView.findViewById(R.id.progressbarFirst)
         var progressbarSecond : ProgressBar = itemView.findViewById(R.id.progressbarSecond)
         var label1 : TextView = itemView.findViewById(R.id.label_1)
