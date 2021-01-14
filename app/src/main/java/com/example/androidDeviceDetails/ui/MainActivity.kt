9ee6605f98e41fa096fda99b831f97bfd40687d1
Kenneth Godwin <kennethgodwin06@gmail.com>
2021-01-14 04:00:34 -0500

    -- Updated Main Activity UI.

--- app/src/main/java/com/example/androidDeviceDetails/ui/MainActivity.kt
+++ app/src/main/java/com/example/androidDeviceDetails/ui/MainActivity.kt
@@ -7,11 +7,13 @@ import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import com.example.androidDeviceDetails.R
+import com.example.androidDeviceDetails.adapters.RecyclerItemClickListener
 import com.example.androidDeviceDetails.controller.ActivityController
 import com.example.androidDeviceDetails.databinding.ActivityMainBinding
 import com.example.androidDeviceDetails.models.MainActivityCookedData
@@ -19,12 +21,11 @@ import com.example.androidDeviceDetails.services.AppService
 import com.example.androidDeviceDetails.utils.PrefManager
 import com.example.androidDeviceDetails.utils.Utils
 
-
 const val permissionCode = 200
 val permissions: Array<String> =
     arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.READ_PHONE_STATE)
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
     companion object {
         const val NAME = "Main Activity"
     }
@@ -44,24 +45,41 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             Utils.addInitialData(this)
             PrefManager.createInstance(this).putBoolean(PrefManager.INITIAL_LAUNCH, true)
         }
-        binding.apply {
-            binding.appInfo.button.setOnClickListener(this@MainActivity)
-            binding.batteryInfo.button.setOnClickListener(this@MainActivity)
-            binding.toLocationActivity.button.setOnClickListener(this@MainActivity)
-            binding.appData.button.setOnClickListener(this@MainActivity)
-            binding.signal.button.setOnClickListener(this@MainActivity)
-        }
-    }
 
+        binding.recyclerView.addOnItemTouchListener(
+            RecyclerItemClickListener(
+                this,
+                binding.recyclerView,
+                object : RecyclerItemClickListener.OnItemClickListener {
+                    val cardsTitles = resources.getStringArray(R.array.card_names)
+                    override fun onItemClick(view: View, position: Int) {
+                        when (view.findViewById<TextView>(R.id.cardTitle).text) {
+                            cardsTitles[0] -> startActivity(
+                                Intent(
+                                    this@MainActivity,
+                                    AppInfoActivity::class.java
+                                )
+                            )
+                            cardsTitles[1] -> startActivity(
+                                Intent(
+                                    this@MainActivity,
+                                    BatteryActivity::class.java
+                                )
+                            )
+                            cardsTitles[2] -> startActivity(
+                                Intent(
+                                    this@MainActivity,
+                                    NetworkUsageActivity::class.java
+                                )
+                            )
+                        }
+                    }
 
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.toLocationActivity -> startActivity(Intent(this, LocationActivity::class.java))
-            R.id.batteryInfo -> startActivity(Intent(this, BatteryActivity::class.java))
-            R.id.appInfo -> startActivity(Intent(this, AppInfoActivity::class.java))
-            R.id.appData -> startActivity(Intent(this, NetworkUsageActivity::class.java))
-            R.id.signal -> startActivity(Intent(this, SignalActivity::class.java))
-        }
+                    override fun onItemLongClick(view: View?, position: Int) {
+                        TODO("do nothing")
+                    }
+                })
+        )
     }
 
     private fun requestPermissions() =
