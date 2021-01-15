9c12a02b2a64cbf8a073da7aa86b8b1185fa29e1
Kenneth Godwin <kennethgodwin06@gmail.com>
2021-01-15 01:52:05 -0500

    --Renamed entity to row. -- Moved data class and dao into one file.

--- app/src/main/java/com/example/androidDeviceDetails/models/networkUsageModels/AppNetworkUsageRow.kt
+++ app/src/main/java/com/example/androidDeviceDetails/models/networkUsageModels/AppNetworkUsageRow.kt
@@ -1,9 +1,7 @@
 package com.example.androidDeviceDetails.models.networkUsageModels
 
 import android.net.NetworkCapabilities
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import androidx.room.*
 import com.example.androidDeviceDetails.collectors.NetworkUsageCollector
 import com.example.androidDeviceDetails.cooker.NetworkUsageCooker
 import com.example.androidDeviceDetails.models.RoomDB
@@ -30,4 +28,35 @@ data class AppNetworkUsageEntity(
     @ColumnInfo(name = "receivedDataWifi ") var receivedDataWifi: Long,
     @ColumnInfo(name = "receivedDataMobile ") var receivedDataMobile: Long
 )
+
+/**
+ * An interface that contains functions to handle database operations.
+ */
+@Dao
+interface AppNetworkUsageDao {
+    /**
+     * Retrieve all the records from [AppNetworkUsageDao]
+     * @return A list of [AppNetworkUsageEntity].
+     */
+    @Query("SELECT * FROM AppNetworkUsageEntity")
+    fun getAll(): List<AppNetworkUsageEntity>
+
+    /**
+     * Retrieve all the records from [AppNetworkUsageDao] between the [startTime] and [endTime]
+     * @param startTime Start Time
+     * @param endTime End Time
+     * @return A list of [AppNetworkUsageEntity].
+     */
+    @Query("SELECT * FROM AppNetworkUsageEntity WHERE timeStamp BETWEEN (:startTime) AND (:endTime)")
+    fun getAllBetween(startTime: Long, endTime: Long): List<AppNetworkUsageEntity>
+
+    @Query("DELETE FROM AppNetworkUsageEntity")
+    fun deleteAll()
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg appNetworkUsageEntity: AppNetworkUsageEntity)
+
+    @Delete
+    fun delete(appNetworkUsageEntity: AppNetworkUsageEntity)
+}
 
--- app/src/main/java/com/example/androidDeviceDetails/models/networkUsageModels/DeviceNetworkUsageRow.kt
+++ app/src/main/java/com/example/androidDeviceDetails/models/networkUsageModels/DeviceNetworkUsageRow.kt
@@ -1,9 +1,7 @@
 package com.example.androidDeviceDetails.models.networkUsageModels
 
 import android.net.NetworkCapabilities
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import androidx.room.*
 import com.example.androidDeviceDetails.collectors.NetworkUsageCollector
 import com.example.androidDeviceDetails.models.RoomDB
 
@@ -24,5 +22,36 @@ data class DeviceNetworkUsageEntity(
     @ColumnInfo(name = "receivedDataWifi ") val receivedDataWifi: Long,
     @ColumnInfo(name = "receivedDataMobile ") val receivedDataMobile: Long
 )
+
+@Dao
+/**
+ * An interface that contains functions to handle database operations.
+ */
+interface DeviceNetworkUsageDao {
+    /**
+     * Retrieve all the records from [DeviceNetworkUsageDao]
+     * @return A list of [DeviceNetworkUsageEntity].
+     */
+    @Query("SELECT * FROM DeviceNetworkUsageEntity")
+    fun getAll(): List<DeviceNetworkUsageEntity>
+
+    /**
+     * Retrieve all the records from [DeviceNetworkUsageDao] between the [startTime] and [endTime]
+     * @param startTime Start Time
+     * @param endTime End Time
+     * @return A list of [DeviceNetworkUsageDao].
+     */
+    @Query("SELECT * FROM DeviceNetworkUsageEntity WHERE timeStamp BETWEEN (:startTime) AND (:endTime)")
+    fun getAllBetween(startTime: Long, endTime: Long): List<DeviceNetworkUsageEntity>
+
+    @Query("DELETE FROM DeviceNetworkUsageEntity")
+    fun deleteAll()
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg deviceNetworkUsageEntity: DeviceNetworkUsageEntity)
+
+    @Delete
+    fun delete(deviceNetworkUsageEntity: DeviceNetworkUsageEntity)
+}
 
 
