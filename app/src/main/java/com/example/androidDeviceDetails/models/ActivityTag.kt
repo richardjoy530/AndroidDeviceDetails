0ac09deb43d50879e4b91d0202ebc8238d051d49
Kenneth Godwin <kennethgodwin06@gmail.com>
2021-01-25 06:59:15 -0500

    --Refactor

--- app/src/main/java/com/example/androidDeviceDetails/models/ActivityTag.kt
+++
@@ -1,9 +0,0 @@
-package com.example.androidDeviceDetails.models
-
-enum class ActivityTag {
-    APP_INFO,
-    BATTERY_DATA,
-    DEVICE_NETWORK_USAGE,
-    LOCATION_DATA,
-    SIGNAL_DATA
-}
--- app/src/main/java/com/example/androidDeviceDetails/models/LayoutType.kt
+++
@@ -1,6 +0,0 @@
-package com.example.androidDeviceDetails.models
-
-enum class LayoutType {
-    PROGRESSBAR_LAYOUT,
-    SINGLE_VALUE_LAYOUT
-}
