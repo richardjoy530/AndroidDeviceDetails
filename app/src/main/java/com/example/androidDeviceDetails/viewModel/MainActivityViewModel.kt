136242407d6e8906f2637a37b03712a5b20aa652
Kenneth Godwin <kennethgodwin06@gmail.com>
2021-01-25 06:47:31 -0500

    --Refactor

--- app/src/main/java/com/example/androidDeviceDetails/viewModel/MainActivityViewModel.kt
+++ app/src/main/java/com/example/androidDeviceDetails/viewModel/MainActivityViewModel.kt
@@ -1,8 +1,6 @@
 package com.example.androidDeviceDetails.viewModel
 
 import android.content.Context
-import android.util.Log
-import android.view.View.GONE
 import android.view.View.VISIBLE
 import com.example.androidDeviceDetails.base.BaseViewModel
 import com.example.androidDeviceDetails.databinding.ActivityMainBinding
@@ -21,14 +19,14 @@ class MainActivityViewModel(private val binding: ActivityMainBinding, val contex
     override fun <T> onDone(outputList: ArrayList<T>) {
         val appInfoList = arrayListOf<AppInfoRaw>()
         val batteryList = arrayListOf<BatteryAppEntry>()
-        val deviceNetworkUsageList = arrayListOf<DeviceNetworkUsageRaw>()
+        val dataUsageList = arrayListOf<DeviceNetworkUsageRaw>()
         val locationList = arrayListOf<LocationDisplayModel>()
         val signalList = arrayListOf<SignalRaw>()
         for (i in 0 until outputList.size) {
             when (outputList[i]) {
                 is AppInfoRaw -> appInfoList.add(outputList[i] as AppInfoRaw)
                 is BatteryAppEntry -> batteryList.add(outputList[i] as BatteryAppEntry)
-                is DeviceNetworkUsageRaw -> deviceNetworkUsageList.add(outputList[i] as DeviceNetworkUsageRaw)
+                is DeviceNetworkUsageRaw -> dataUsageList.add(outputList[i] as DeviceNetworkUsageRaw)
                 is LocationDisplayModel -> locationList.add(outputList[i] as LocationDisplayModel)
                 is SignalRaw -> signalList.add(outputList[i] as SignalRaw)
             }
@@ -36,78 +34,57 @@ class MainActivityViewModel(private val binding: ActivityMainBinding, val contex
         binding.root.post {
             if (appInfoList.isNotEmpty()) updateAppInfoCard(appInfoList)
             if (batteryList.isNotEmpty()) updateBatteryCard(batteryList)
-            if (deviceNetworkUsageList.isNotEmpty()) updateDeviceNetworkUsageCard(
-                deviceNetworkUsageList
-            )
+            if (dataUsageList.isNotEmpty()) updateDeviceNetworkUsageCard(dataUsageList)
             if (locationList.isNotEmpty()) updateLocationDataCard(locationList)
             if (signalList.isNotEmpty()) updateSignalDataCard(signalList)
         }
-
     }
 
     private fun updateBatteryCard(outputList: ArrayList<BatteryAppEntry>) {
         val totalDrop = outputList.sumBy { it.drop }
-        outputList.sortBy { it.drop }
-        binding.batteryInfo.app1.visibility = GONE
-        binding.batteryInfo.app2.visibility = GONE
-        binding.batteryInfo.app3.visibility = GONE
-        binding.batteryInfo.app1Icon.visibility = GONE
-        binding.batteryInfo.app2Icon.visibility = GONE
-        binding.batteryInfo.app3Icon.visibility = GONE
+        outputList.sortByDescending { it.drop }
         if (outputList.size > 2) {
             (outputList[2].drop.toString() + "%").also { binding.batteryInfo.app1.text = it }
             binding.batteryInfo.app1.visibility = VISIBLE
             binding.batteryInfo.app1Icon.visibility = VISIBLE
-            binding.batteryInfo.app1Icon.setImageDrawable( Utils.getApplicationIcon(outputList[2].packageId))
+            binding.batteryInfo.app1Icon.setImageDrawable(Utils.getApplicationIcon(outputList[2].packageId))
         }
         if (outputList.size > 1) {
             (outputList[1].drop.toString() + "%").also { binding.batteryInfo.app2.text = it }
             binding.batteryInfo.app2.visibility = VISIBLE
             binding.batteryInfo.app2Icon.visibility = VISIBLE
-
-            binding.batteryInfo.app2Icon.setImageDrawable( Utils.getApplicationIcon(outputList[1].packageId))
+            binding.batteryInfo.app2Icon.setImageDrawable(Utils.getApplicationIcon(outputList[1].packageId))
         }
         if (outputList.size > 0) {
             (outputList[0].drop.toString() + "%").also { binding.batteryInfo.app3.text = it }
             binding.batteryInfo.app3.visibility = VISIBLE
             binding.batteryInfo.app3Icon.visibility = VISIBLE
-            binding.batteryInfo.app3Icon.setImageDrawable( Utils.getApplicationIcon(outputList[0].packageId))
+            binding.batteryInfo.app3Icon.setImageDrawable(Utils.getApplicationIcon(outputList[0].packageId))
         }
         binding.batteryInfo.mainValue.text = totalDrop.toString()
     }
 
     private fun updateAppInfoCard(outputList: ArrayList<AppInfoRaw>) {
         val systemAppsCount = outputList.filter { it.isSystemApp }.size
-        val systemAppsProgress = ceil(
-            ((systemAppsCount).toDouble().div(outputList.size).times(100))
-        ).toInt()
         val userAppsCount = outputList.size - systemAppsCount
-        val userAppsProgress = ceil(
-            ((userAppsCount).toDouble().div(outputList.size).times(100))
-        ).toInt()
+        val systemAppsProgress = graphCalculator(systemAppsCount, outputList.size)
+        val userAppsProgress = graphCalculator(userAppsCount, outputList.size)
         binding.appInfo.label1Value.text = systemAppsCount.toString()
         binding.appInfo.label2Value.text = userAppsCount.toString()
         binding.appInfo.progressbarFirst.progress = systemAppsProgress + userAppsProgress
         binding.appInfo.progressbarSecond.progress = userAppsProgress
-        Log.d("MainViewModel", "AppInfo  ")
     }
 
     private fun updateDeviceNetworkUsageCard(outputList: ArrayList<DeviceNetworkUsageRaw>) {
         val wifiData =
-            (outputList.first().transferredDataWifi + outputList.first().receivedDataWifi) / 1024.0.pow(
-                2.toDouble()
-            )
+            (outputList.first().transferredDataWifi + outputList.first().receivedDataWifi) /
+                    1024.0.pow(2.toDouble())
         val cellularData =
-            (outputList.first().transferredDataMobile + outputList.first().transferredDataMobile) / 1024.0.pow(
-                2.toDouble()
-            )
+            (outputList.first().transferredDataMobile + outputList.first().transferredDataMobile) /
+                    1024.0.pow(2.toDouble())
         val total = wifiData + cellularData
-        val wifiDataProgress = ceil(
-            ((wifiData).div(total).times(100))
-        ).toInt()
-        val cellularDataProgress = ceil(
-            ((cellularData).div(total).times(100))
-        ).toInt()
+        val wifiDataProgress = graphCalculator(wifiData.toInt(), total.toInt())
+        val cellularDataProgress = graphCalculator(cellularData.toInt(), total.toInt())
         binding.networkUsage.label1Value.text =
             Utils.getFileSize(outputList.first().transferredDataWifi + outputList.first().receivedDataWifi)
         binding.networkUsage.label2Value.text =
@@ -121,8 +98,10 @@ class MainActivityViewModel(private val binding: ActivityMainBinding, val contex
     }
 
     private fun updateSignalDataCard(outputList: ArrayList<SignalRaw>) {
-        binding.signalData.pointerCellularSpeedometer.speedTo(50F,1000)
-        binding.signalData.pointerWifiSpeedometer.speedTo(70F,1000)
-
+        binding.signalData.pointerCellularSpeedometer.speedTo(50F, 1000)
+        binding.signalData.pointerWifiSpeedometer.speedTo(70F, 1000)
     }
+
+    private fun graphCalculator(dataSize: Int, total: Int) =
+        ceil(((dataSize).toDouble().div(total).times(100))).toInt()
 }
